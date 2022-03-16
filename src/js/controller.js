23f19604c33f0747915490a552ddb210fc1a52c6
ctrlgear <smolenschi.mihai@gmail.com>
2022-03-16 23:02:16 +0200

    add event handlers in mvc: publisher-subscriber pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -21,13 +21,11 @@ const controlRecipes = async function () {
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err.message);
+    console.log(err);
   }
 };
 
-["hashchange", "load"].forEach((ev) =>
-  window.addEventListener(ev, controlRecipes)
-);
-
-// window.addEventListener("hashchange", showRecipe);
-// window.addEventListener("load", showRecipe);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
