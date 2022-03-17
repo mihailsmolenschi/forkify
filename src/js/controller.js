d349f09d8035cf4405a7a8da6c6438fa79b1c133
ctrlgear <smolenschi.mihai@gmail.com>
2022-03-17 19:30:03 +0200

    add pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,8 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
-import searchView from "./searchView.js";
+import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
+import paginationView from "./views/paginationView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
@@ -41,16 +42,27 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render the initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  // 1) Render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render new pagination buttons
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
-
-console.log("Implementing pagination");
