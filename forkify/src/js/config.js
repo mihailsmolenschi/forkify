9d8178e122a8600661e97ed105650c6bb4c4f40c
Mihai <smolenschi.mihai@gmail.com>
2022-09-29 14:46:38 +0300

    initial commit

---
+++ forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "c3337c1f-74c5-425a-ac28-fc94c108bd80";
+export const MODAL_CLOSE_SEC = 2.5;
